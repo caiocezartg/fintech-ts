340af1a24b06aaf4082f35d99996d6363ab440ca
Caio Cezar <caiocezartg@gmail.com>
2023-09-28 02:13:12 -0300

    feat: add Sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import DataRange from "../DateRange";
 import Months from "../Months";
 
 const Header = () => {
+  const [title, setTitle] = useState("Resumo");
+
   return (
     <header className="mb">
-      <div className="mb">
+      <div className="daterange mb">
         <DataRange />
+        <h1 className="box bg-3">{title}</h1>
       </div>
       <Months />
     </header>
